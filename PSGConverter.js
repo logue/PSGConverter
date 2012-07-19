93aaa960b88b81d86f27a0bbaa77ec1c0f8affef
logue <logue@hotmail.co.jp>
2012-07-19 16:58:13 +0900

    Fixed for Firefox. IE still not working...

--- PSGConverter.js
+++ PSGConverter.js
@@ -225,39 +225,30 @@
 	// Version 1.2 Apr. 8 2004 written by MIZUTANI Tociyuki
 	// Copyright 2003-2004 MIZUTANI Tociyuki
 	// http://tociyuki.cool.ne.jp/archive/base64.html
-	//
-	// This code is free software; you can redistribute it and/or
-	// modify it under the terms of the GNU Library General Public
-	// License as published by the Free Software Foundation; either
-	// version 2 of the License, or (at your option) any later version.
-	//
-	// usage:
-	// base64 = base64encode(string)	Encode a string.
-	// string = base64decode(base64)	Decode a base64 string.
-	//
-	// caution:
-	// 1) Wide characters like japanese kanji are not supported. Use only in Latin-1.
-
-	var base64list = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=';
 
 	function base64encode(s){
-		var t = '', p = -6, a = 0, i = 0, v = 0, c;
-		while ( (i < s.length) || (p > -6) ) {
-			if ( p < 0 ) {
-				if ( i < s.length ) {
-					c = s.charCodeAt(i++);
-					v += 8;
-				} else {
-					c = 0;
+		if (window.btoa){
+			return btoa(s);
+		}else{
+			var base64list = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=';
+			var t = '', p = -6, a = 0, i = 0, v = 0, c;
+			while ( (i < s.length) || (p > -6) ) {
+				if ( p < 0 ) {
+					if ( i < s.length ) {
+						c = s.charCodeAt(i++);
+						v += 8;
+					} else {
+						c = 0;
+					}
+					a = ((a&255)<<8)|(c&255);
+					p += 8;
 				}
-				a = ((a&255)<<8)|(c&255);
-				p += 8;
+				t += base64list.charAt( ( v > 0 )? (a>>p)&63 : 64 )
+				p -= 6;
+				v -= 6;
 			}
-			t += base64list.charAt( ( v > 0 )? (a>>p)&63 : 64 )
-			p -= 6;
-			v -= 6;
+			return t;
 		}
-		return t;
 	}
 
 	$(document).ready(function(){
@@ -265,16 +256,18 @@
 			var $this = $(this);
 			var MML = mml_sanitize($this.contents()[0].nodeValue);	// MML配列
 			var inst = $this.data().inst;	// 楽器
-			var url = 'data:audio/x-midi;base64,'+base64encode(mabimml_genmidi({"inst":inst,"mml":MML}) );
+			var url = 'data:audio/midi;base64,'+base64encode(mabimml_genmidi({"inst":inst,"mml":MML}) );
 			$this.before([
-//				'<a class="btn btn-small mml-debug" href="'+url+'">debug.mid</a>',
-				'<object classid="clsid:02BF25D5-8C17-4B23-BC80-D3488ABDDC6B"  type="application/x-quicktimeplayer" width="320" height="16" class="mml-player">',
-					'<param name="controller" value="true" >',
-					'<param name="autoplay" value="true">',
-					'<param name="src" value="'+url+'" >',
-					'<object type="video/quicktime" width="320" height="16" data="'+url+'">',
-						'<param name="controller" value="true" >',
-						'<param name="autoplay" value="false">',
+				'<a class="btn btn-small mml-debug" href="'+url+'">'+$this.attr('title')+'.mid</a>',
+				'<object classid="clsid:02BF25D5-8C17-4B23-BC80-D3488ABDDC6B" codebase="http://www.apple.com/qtactivex/qtplugin.cab" width="320" height="16" class="mml-player">',
+					'<param name="controller" value="true" />',
+					'<param name="autoplay" value="true" />',
+					'<param name="src" value="'+url+'" />',
+					'<param name="pluginspage" value="http://www.apple.com/quicktime/download/" />',
+					'<object type="audio/midi" width="320" height="16" data="'+url+'">',
+						'<param name="controller" value="true" />',
+						'<param name="autoplay" value="false" />',
+						'<param name="pluginspage" value="http://www.apple.com/quicktime/download/" />',
 					'</object>',
 				'</object>'
 			].join("\n"));
